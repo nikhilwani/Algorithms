bb622f1f2b5b76186c1127c13756efa7505190f7
Nikhil Wani <nikhilwani@outlook.com>
2022-01-23 10:34:18 -0800

    Time: 28 ms (85.18%), Space: 14.3 MB (19.62%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,48 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        if s == None or len(s) == 0:
+            return s
+        
+        num = 0
+        currstr = ""
+        
+        numstack = []
+        strstack = []
+        
+        for i in range(len(s)):
+            print("i", i)
+            c = s[i]
+            
+            if c.isdigit():
+                num = ((num * 10) + int(c) - int('0'))
+                print("nnumm")
+                print(num)
+            
+            elif (c == '['):
+                print("num:")
+                print(num)
+                numstack.append(num)
+                strstack.append(currstr)
+                num = 0
+                currstr = ""
+            
+            elif c == ']':
+                print("here")
+                
+                times = numstack.pop()
+                newstring = ""
+                
+                for i in range (times):
+                    newstring = newstring + currstr
+                
+                parent = strstack.pop()
+                currstr = parent + newstring 
+                
+            else:
+                currstr = currstr + c
+                
+        
+        
+        return currstr
+        
