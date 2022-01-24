f8bc4e0ab9c836bdc89cb77d0052abc9b8f6bf36
Nikhil Wani <nikhilwani@outlook.com>
2022-01-24 05:37:45 -0800

    Time: 162 ms (5.16%), Space: 14.4 MB (54.84%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,7 +1,49 @@
 class Solution:
+    
+    color = 0 
+    dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+    m = 0
+    n = 0 
+    
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         
+        # BFS 
+        if image == None or len(image) == 0 or image[sr][sc] == newColor:
+            return image
+         
+        self.m = len(image)
+        self.n = len(image[0])
         
+        self.color = image[sr][sc]
+        self.dfs(image, sr, sc, newColor)  
+        
+        return image
+
+    def dfs(self, image, r, c, newColor):
+        
+        #base
+        if r < 0 or c < 0 or r == self.m or c == self.n or image[r][c] != self.color: 
+            return
+        
+        # logic
+        image[r][c] = newColor
+        
+        for dirr in self.dirs:
+            nr = r + dirr[0]
+            nc = c + dirr[1]
+            self.dfs(image, nr, nc, newColor)
+    
+
+
+
+'''
+
+
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        
+        # BFS 
         if image == None or len(image) == 0 or image[sr][sc] == newColor:
             return image
          
@@ -29,3 +71,4 @@ class Solution:
         
         
         return image
+'''
