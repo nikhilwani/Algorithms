9c62e7e7e5d29fffd951453b74149c471e968d04
Nikhil Wani <nikhilwani@outlook.com>
2022-01-23 21:43:58 -0800

    Time: 32 ms (62.56%), Space: 14.1 MB (92.96%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,4 +1,60 @@
 class Solution:
+    
+    # Global variable
+    i = 0
+    
+    def decodeString(self, s: str) -> str:
+        if s == None or len(s) == 0:
+            return s
+        
+        num = 0
+        currstr = ""
+        
+        numstack = []
+        strstack = []
+        
+        while self.i < len(s):
+            #print("i", self.i)
+            c = s[self.i]
+            
+            if c.isdigit():
+                num = ((num * 10) + int(c))
+                #print("num formed",num)
+                self.i = self.i + 1
+            
+            # It is the child when '[' is detected.
+            elif (c == '['):
+                
+                self.i = self.i + 1
+                child = self.decodeString(s)
+                newstr = ""
+                
+                for k in range(num):
+                    newstr = newstr + child
+                    
+                currstr = currstr + newstr
+                num = 0
+                
+                
+            elif c == ']':
+                self.i = self.i + 1
+                return currstr
+                
+            else:
+                currstr = currstr + c
+                self.i = self.i + 1
+        
+        return currstr
+        
+
+
+
+'''
+# Iterative 
+
+class Solution:
+    
+    
     def decodeString(self, s: str) -> str:
         
         if s == None or len(s) == 0:
@@ -15,7 +71,7 @@ class Solution:
             c = s[i]
             
             if c.isdigit():
-                num = ((num * 10) + int(c) )
+                num = ((num * 10) + int(c))
                 print("num formed",num)
             
             elif (c == '['):
@@ -43,4 +99,4 @@ class Solution:
         
         
         return currstr
-        
+'''        
