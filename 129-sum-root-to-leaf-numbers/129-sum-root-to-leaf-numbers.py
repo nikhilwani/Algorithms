f33e805f9252b6ef3ede1f4055a9d389df57dc62
Nikhil Wani <nikhilwani@outlook.com>
2022-03-08 21:52:05 -0800

    Time: 24 ms (98.84%), Space: 13.9 MB (82.23%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -7,10 +7,77 @@
 
 
 class Solution:
+#     global rootsum
+#     rootsum = 0 
     
+#     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+#         # this works! We are not assigning value to a global variable.
+#         #print(rootsum)
+        
+#         if root == None:
+#             return 0
+        
+#         self.helper(root, 0)
+        
+#         return rootsum
+        
+        
+#     def helper(self, root, cursum):
+#         #print(rootsum)
+#         # since we are assinging a global variable.
+#         global rootsum
+        
+#         if root == None:
+#             return 
+        
+#         cursum = 10 * cursum + root.val
+        
+        
+#         self.helper(root.left, cursum)
+        
+#         if root.left == None and root.right == None:
+#             rootsum = rootsum + cursum
+        
+        
+#         self.helper(root.right, cursum)
+            
+            
+# '''  
+#     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        
+#         rootsum = 0 
+
+#         if root == None:
+#             return 0
+        
+        
+        
+#         def helper(self, root, cursum):
+
+#             if root == None:
+#                 return 
+
+#             cursum = 10 * cursum + root.val
+            
+#             helper(self, root.left, cursum)
+
+#             if root.left == None and root.right == None:
+#                 rootsum = rootsum + cursum
+
+
+#             helper(self, root.right, cursum)
+        
+
+#         helper(self, root, 0)
+        
+#         return rootsum
+        
+# '''
+        
+        
+  
     #global rootsum
-    
-    
+
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         
         self.rootsum = 0 
@@ -24,7 +91,7 @@ class Solution:
         
         
     def helper(self, root, cursum):
-        print(self.rootsum)
+        
         if root == None:
             return 
         
@@ -38,3 +105,4 @@ class Solution:
         
         
         self.helper(root.right, cursum)
+           
